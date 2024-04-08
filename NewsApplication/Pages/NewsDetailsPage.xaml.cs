405c8d58a432bf804720e148e473567a956703c1
HasanGhavisheh <ecma.work@gmail.com>
2024-04-08 09:36:08 +0330

    Add Share Button For News

--- NewsApplication/Pages/NewsDetailsPage.xaml.cs
+++ NewsApplication/Pages/NewsDetailsPage.xaml.cs
@@ -4,11 +4,22 @@ namespace NewsApplication.Pages;
 
 public partial class NewsDetailsPage : ContentPage
 {
+    private string _url;
 	public NewsDetailsPage(Article article)
 	{
 		InitializeComponent();
 		Image.Source = article.Image;
 		Title.Text = article.Title;
 		Description.Text = article.Description;
+		_url = article.Url;
 	}
+
+    private async void ShareBtn_OnClicked(object? sender, EventArgs e)
+    {
+        await Share.RequestAsync(new ShareTextRequest
+        {
+			Uri = _url,
+			Text = "Share"
+        });
+    }
 }
